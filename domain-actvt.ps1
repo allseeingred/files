16bfaff999f2b7a2c1adb64a19f0ac0c840525ee
Sir Red <pretzelkoehler@gmail.com>
2023-09-13 02:25:53 -0700

    Update domain-actvt.ps1

--- domain-actvt.ps1
+++ domain-actvt.ps1
@@ -2,3 +2,5 @@ Invoke-WebRequest -uri "https://github.com/massgravel/Microsoft-Activation-Scrip
 Expand-Archive .\MAS.zip
 cd .\MAS\Microsoft-Activation-Scripts-master\MAS\All-In-One-Version\
 Invoke-WebRequest -uri "https://www.box.com/index.php?rm=box_download_shared_file&shared_name=qrmkewit9ty6ah0qzk0vncyyrwo8hei9&file_id=f_747809716860" -outfile ClipUp.exe
+.\MAS_AIO.cmd /KMS38
+exit
